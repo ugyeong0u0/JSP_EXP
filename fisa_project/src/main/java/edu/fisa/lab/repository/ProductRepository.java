34b9ad1573769a91aba01c739b63c93151928adb
DLKJFWOEK <112544126+DLKJFWOEK@users.noreply.github.com>
2024-01-09 08:31:36 +0900

    Update ProductRepository.java

--- fisa_project/src/main/java/edu/fisa/lab/repository/ProductRepository.java
+++ fisa_project/src/main/java/edu/fisa/lab/repository/ProductRepository.java
@@ -4,9 +4,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.fisa.lab.model.domain.Product;
-
+	
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	@Query("select productId from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectProductIdById(@Param("customerid") Long customerid);
 	
+	@Query("select productName from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectProductNameById(@Param("customerid") Long customerid);
+	
+	@Query("select price from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectPriceById(@Param("customerid") Long customerid);
+	@Query("select brand from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectBrandById(@Param("customerid") Long customerid);
+	@Query("select size from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectSizeById2(@Param("customerid") Long customerid);
+	@Query("select category from Product p inner join Customer c on p.customerId =c.customerid "
+			+ "and where c.customerId=:customerid")
+	int selectCategoryById2(@Param("customerid") Long customerid);
+		
 }
