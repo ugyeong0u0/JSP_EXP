d238d34892e08ace62648c43594eb7b81c66f71f
KimEoJin <ejk5701jin@gmail.com>
2024-01-11 11:40:19 +0900

    commit

--- fisa_project/src/main/java/edu/fisa/lab/repository/ProductRepository.java
+++ fisa_project/src/main/java/edu/fisa/lab/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package edu.fisa.lab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.fisa.lab.model.domain.Product;
@@ -8,25 +10,30 @@ import edu.fisa.lab.model.domain.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	@Query("select productId from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectProductIdById(@Param("customerid") Long customerid);
+//	@Query("select productId from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectProductIdById(@Param("customerid") Long customerid);
+//	
+//	@Query("select productName from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectProductNameById(@Param("customerid") Long customerid);
+//	
+//	@Query("select price from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectPriceById(@Param("customerid") Long customerid);
+//	
+//	@Query("select brand from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectBrandById(@Param("customerid") Long customerid);
+//	
+//	@Query("select size from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectSizeById2(@Param("customerid") Long customerid);
+//	
+//	@Query("select category from Product p inner join Customer c on p.customerId =:c.customerid "
+//			+ "and where c.customerId=:customerid")
+//	Long selectCategoryById2(@Param("customerid") Long customerid);
 	
-	@Query("select productName from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectProductNameById(@Param("customerid") Long customerid);
-	
-	@Query("select price from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectPriceById(@Param("customerid") Long customerid);
-	@Query("select brand from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectBrandById(@Param("customerid") Long customerid);
-	@Query("select size from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectSizeById2(@Param("customerid") Long customerid);
-	@Query("select category from Product p inner join Customer c on p.customerId =c.customerid "
-			+ "and where c.customerId=:customerid")
-	int selectCategoryById2(@Param("customerid") Long customerid);
+
 		
 }
